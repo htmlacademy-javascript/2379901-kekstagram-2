72435b0599b8969c49b92a0ebb7d391f3db870f7
Данила <danila.designer.inc@yandex.ru>
2024-10-15 22:31:27 +0300

    вносит правки

--- js/main.js
+++ js/main.js
@@ -37,42 +37,37 @@ const getRandomInteger = (a, b) => {
 };
 
 //Сообщение коментария
-const createComment = () => {
-  let id = 1;
+const createComment = (_, index) => {
   const indexMessage = getRandomInteger(0, MESSAGES.length - 1);
   const indexName = getRandomInteger(0, NAMES.length - 1);
+  const idAvatar = getRandomInteger(1, 6);
 
-  return () => {
-    const comment = {};
-    const idAvatar = getRandomInteger(1, 6);
-    comment.id = id;
-    comment.avatar = `img/avatar-${idAvatar()}.svg`;
-    comment.message = `${MESSAGES[indexMessage()]}. ${MESSAGES[indexMessage()]}`;
-    comment.name = `${NAMES[indexName()]}`;
-    id++;
-
-    return comment;
+  const comment = {
+    id: index + 1,
+    avatar: `img/avatar-${idAvatar()}.svg`,
+    message: `${MESSAGES[indexMessage()]}. ${MESSAGES[indexMessage()]}`,
+    name: `${NAMES[indexName()]}`
   };
+
+  return comment;
 };
 
 const numberComment = getRandomInteger(0, 30);
 const numberLike = getRandomInteger(15, 200);
+const arrayLength = 25;
 
-const createPhoto = () => {
-  let id = 1;
-
-  return () => {
-    const photo = {};
-    photo.id = id;
-    photo.url = `photos/${id}.jpg`;
-    photo.description = `Фото №${id}`;
-    photo.likes = numberLike();
-    photo.comment = Array.from({length : numberComment()}, createComment());
-    id++;
-    return photo;
+const createPhoto = (_, index) => {
+  const photo = {
+    id: index + 1,
+    url: 'photos/${index + 1}.jpg',
+    description: 'Фото №${index + 1}',
+    likes: numberLike(),
+    comment: Array.from({length : numberComment()}, createComment)
   };
+
+  return photo;
 };
 
-const photoArray = Array.from({length : 25}, createPhoto());
+const photoArray = Array.from({length : arrayLength}, createPhoto);
 
 console.log(photoArray);
